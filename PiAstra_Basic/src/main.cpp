7ff144aead098447ad8fd0e48fa905a95d293299
Nick Fox-Gieg <nick@fox-gieg.com>
2022-04-28 10:52:04 -0400

    settings

--- PiAstra_Basic/src/main.cpp
+++ PiAstra_Basic/src/main.cpp
@@ -1,10 +1,11 @@
 #include "ofMain.h"
 #include "ofApp.h"
 
-int main()
-{
+int main() {
+
 	// Since we are going to see 2 streams, 
 	// let's create a double sized 640 width window
 	ofSetupOpenGL(640 * 2, 480, OF_WINDOW);
 	ofRunApp(new ofApp());
+	
 }
--- PiAstra_Basic/src/ofApp.cpp
+++ PiAstra_Basic/src/ofApp.cpp
@@ -1,30 +1,40 @@
 #include "ofApp.h"
 
 //------------------------------------------------------------------------
-void ofApp::setup()
-{
-	astraCam.setup();
-	astraCam.setRegistration(true);
-	astraCam.initSelectedStreams(true, true, false);
+void ofApp::setup() {
+    XML.loadFile("settings.xml");
+
+	width = XML.getValue("settings:width", 640);
+	height = XML.getValue("settings:height", 480);
+	fps = XML.getValue("settings:fps", 30);;
+	doDepth = ofToBool(XML.getValue("settings:doDepth", "true"));
+	doColor = ofToBool(XML.getValue("settings:doColor", "true")) ;
+	doIr = ofToBool(XML.getValue("settings:doIr", "false")) ;
+	registered = ofToBool(XML.getValue("settings:registered", "true"));
+
+	astraCam.setup(width, height);
+	astraCam.setRegistration(registered);
+	astraCam.initSelectedStreams(doColor, doDepth, doIr); // color, depth, point (?)
 }
+
 //------------------------------------------------------------------------
-void ofApp::update()
-{
+void ofApp::update() {
 	astraCam.update();
-	if (astraCam.isFrameNew())
-	{
+	if (astraCam.isFrameNew()) {
 		// Do something fun.
 	}
 }
+
 //------------------------------------------------------------------------
-void ofApp::draw()
-{
-	astraCam.drawDepth(0, 0);
-	astraCam.draw(640, 0);
+void ofApp::draw() {
+	if (doDepth) astraCam.drawDepth(0, 0);
+	if (doColor || doIr) {
+		astraCam.draw(640, 0);
+	}
 }
+
 //------------------------------------------------------------------------
-void ofApp::keyPressed(int key)
-{
+void ofApp::keyPressed(int key) {
 	// Poll events here!
 }
 //------------------------------------------------------------------------
